1b21520940e6b1da7d25bceafd97811c65c0e497
Matt Marzano <122555133+mattmarz94@users.noreply.github.com>
2023-09-24 19:27:05 -0400

    added a function to the fetch method to retriece data and display that data in the HTML.

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-fetch("https://www.boredapi.com/api/activity")
-      .then(response => response.json())
-      .then(data => console.log(data))
+const activityBtn = document.getElementById("activityBtn")
+
+activityBtn.addEventListener("click", function() {
+        fetch("https://www.boredapi.com/api/activity")
+              .then(response => response.json())
+              .then(data => {
+                console.log(data)
+                document.getElementById("random-activity").textContent = data.activity
+                })
+      })
