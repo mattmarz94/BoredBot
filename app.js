50ed2769f1764942fba6a39ffc8c3bf6b299f27b
Matt Marzano <122555133+mattmarz94@users.noreply.github.com>
2023-09-24 19:37:38 -0400

    removed the console.log from the fetch, and refined the eventListener function.

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-const activityBtn = document.getElementById("activityBtn")
-
-activityBtn.addEventListener("click", function() {
+document.getElementById("activityBtn").addEventListener("click", function() {
         fetch("https://www.boredapi.com/api/activity")
               .then(response => response.json())
               .then(data => {
-                console.log(data)
                 document.getElementById("random-activity").textContent = data.activity
                 })
       })
