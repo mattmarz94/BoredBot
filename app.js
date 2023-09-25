6d1626300d9c4fbcdb98eeb6cc07c9e91b49ed18
Matt Marzano <122555133+mattmarz94@users.noreply.github.com>
2023-09-24 21:09:17 -0400

    added funky styling in the CSS and used the methods in JS to create CSS elements.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-document.getElementById("activityBtn").addEventListener("click", function() {
-        fetch("https://www.boredapi.com/api/activity")
-              .then(response => response.json())
-              .then(data => {
-                document.getElementById("random-activity").textContent = data.activity
-                })
-      })
+document.getElementById("get-activity").addEventListener("click", function() {
+    fetch("https://www.boredapi.com/api/activity")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("activity").textContent = data.activity
+            document.getElementById("title").classList.add("shake")
+            document.body.classList.add("background-change")
+        })
+    })
